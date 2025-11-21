c6c46f8bebf8091c28b9ce01ef407ac9edbc46d4
Erick-P-Calauro <erickcalauro@gmail.com>
2025-11-21 17:30:42 -0400

    feat: Implementação de ControleBiblioteca (Contribuição do Davi)

--- src/main/java/com/ufms/controllers/ControleBiblioteca.java
+++ src/main/java/com/ufms/controllers/ControleBiblioteca.java
@@ -1,8 +1,8 @@
 package com.ufms.controllers;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.ufms.models.ItemBiblioteca;
 import com.ufms.models.IEmprestimo;
 import com.ufms.models.OperacoesBiblioteca;
@@ -12,75 +12,98 @@ public class ControleBiblioteca implements OperacoesBiblioteca {
     private List<ItemBiblioteca> itens;
     private List<IEmprestimo> historico;
 
-    ControleBiblioteca() {
-        itens = new ArrayList<ItemBiblioteca>();
-        historico = new ArrayList<IEmprestimo>();
+    public ControleBiblioteca() {
+        this.itens = new ArrayList<>();
+        this.historico = new ArrayList<>();
     }
+
 
     @Override
     public void registrarEmprestimo(IEmprestimo emprestimo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'registrarEmprestimo'");
+    	this.historico.add(emprestimo);
+        System.out.println("Empréstimo registrado no histórico: " + emprestimo.toString());
     }
 
     @Override
     public void registrarDevolucao(IEmprestimo emprestimo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'registrarDevolucao'");
+
+    	this.historico.add(emprestimo);
+        System.out.println("Devolução registrada no histórico: " + emprestimo.toString());
+
+ 
     }
 
     @Override
     public boolean verificarValidade(IEmprestimo emprestimo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'verificarValidade'");
+        LocalDateTime dataAtual = LocalDateTime.now();
+        return emprestimo.getFimEmprestimo().isAfter(dataAtual);
     }
+
 
     @Override
     public void adicionarItem(ItemBiblioteca item) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'adicionarItem'");
+           this.itens.add(item);
+        
     }
 
     @Override
     public void editarItem(int codigo, ItemBiblioteca itemNovo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'editarItem'");
+        for (int i = 0; i < this.itens.size(); i++) {
+            ItemBiblioteca itemAtual = this.itens.get(i);
+            
+            if (itemAtual.getCodigo() == codigo) {
+                this.itens.set(i, itemNovo);
+                
+              
+                System.out.println("Item código " + codigo + " editado com sucesso.");
+                return;
+            }
+        }
+        
+        System.out.println("Item código " + codigo + " não encontrado para edição.");
     }
 
     @Override
     public ItemBiblioteca buscarItem(int codigo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscarItem'");
+        for (ItemBiblioteca item : this.itens) {
+            if (item.getCodigo() == codigo) {
+                return item;
+            }
+        }
+       return null;
     }
 
     @Override
     public List<ItemBiblioteca> listarItens() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listarItens'");
+    	return this.itens;
     }
 
     @Override
     public void deletarItem(int codigo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deletarItem'");
+        for (int i = 0; i < this.itens.size(); i++) {
+            ItemBiblioteca itemAtual = this.itens.get(i);
+            
+            if (itemAtual.getCodigo() == codigo) { 
+                this.itens.remove(i);              
+                return;
+            }
+        }
     }
-
     @Override
     public void realizarEmprestimo(ItemBiblioteca item) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'realizarEmprestimo'");
+    	// TODO Auto-generated method stub
+    	throw new UnsupportedOperationException("Unimplemented method 'realizarEmprestimo'");
     }
+
 
     @Override
     public void realizarDevolucao(ItemBiblioteca item) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'realizarDevolucao'");
+    	// TODO Auto-generated method stub
+    	throw new UnsupportedOperationException("Unimplemented method 'realizarDevolucao'");
     }
-
     @Override
     public void verificarHistorico(ItemBiblioteca item) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'verificarHistorico'");
+    	// TODO Auto-generated method stub
+    	throw new UnsupportedOperationException("Unimplemented method 'verificarHistorico'");
     }
-
 }
